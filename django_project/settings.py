a70c14b327055abaa0d56a3c8d939b28f7764477
KaelanUMT <kae9car@gmail.com>
2022-10-06 16:50:39 -0600

    user accounts

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,3 +123,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Login/Logout redirect
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
